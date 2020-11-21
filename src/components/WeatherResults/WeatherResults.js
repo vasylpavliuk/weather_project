c2e410d08e3b7470dd85c4671f48a57dc382b09a
vasylpavliuk <vasylpavliuk007@gmail.com>
2020-11-21 23:20:57 +0200

    version saved with react hooks

--- src/components/WeatherResults/WeatherResults.js
+++ src/components/WeatherResults/WeatherResults.js
@@ -9,7 +9,10 @@ const WeatherResults = (props) => {
             <div>
                 <p>{data.dt}</p>
                 <h2>{data.name}, {data.sys.country}</h2>
-                <h2>{data.main.temp.toFixed(0)}°C</h2>
+
+                <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
+                <h2> {data.main.temp.toFixed(0)}°C</h2>
+                <p>Відчувається як {data.main.feels_like.toFixed(0)}°C. {data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)}</p>
             </div>
         
     }
